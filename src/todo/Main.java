423d17ddd75286d27580e3107520f1a32d42b172
James Hatfield <james.hatfield17@icloud.com>
2025-06-13 20:49:41 +0100

    Added code to allow users to add To Do instance

--- src/todo/Main.java
+++ src/todo/Main.java
@@ -14,7 +14,7 @@ public class Main {
         while (true) {
             System.out.println("Add a new item (add), view an existing item (view), or quit (quit)");
             String continuePrompt = input.next();
-
+            input.nextLine();
             // Exit criteria of code
             if (continuePrompt.toLowerCase().contains("quit")) {
                 break;
@@ -22,15 +22,27 @@ public class Main {
 
             switch (continuePrompt.toLowerCase()) {
                 case "add":
-                    System.out.println("to be continued");
+                    System.out.println("Add a Description");
+                    String description = input.nextLine();
+                    System.out.println("Add a Priority");
+                    String priority = input.next();
+                    System.out.println("Is this completed?");
+                    char completed = input.next().charAt(0);
+                    add(description, priority, completed);
                     break;
+
                 case "view":
                     System.out.println("To also be continued");
                     break;
+
                 default:
                     System.out.println("Please enter a valid argument");
             }
         }
+    }
 
+    // Method for when "Add" is entered by user
+    public static void add(String description, String priority, char completed) {
+        var newToDo = new ToDo(description, priority, completed);
     }
 }
