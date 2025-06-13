305e2985acca9ad48618bfe77eef33f89e471c90
James Hatfield <james.hatfield17@icloud.com>
2025-06-13 16:54:39 +0100

    Added initial flow of program, allows user to create a to do entry, set description and priority, tracks ID

--- src/todo/Main.java
+++ src/todo/Main.java
@@ -1,9 +1,35 @@
 package todo;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Global variable for ID tracking
+        int id = 0;
+        // Creating an instance
+        ToDo toDo1 = new ToDo(id + 1);
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to our To Do tracker!");
+        System.out.println();
+
+        // Initial prompt for user
+        System.out.println("Please create your first To Do!");
+
+        System.out.println("Enter a task description");
+        String description = input.nextLine();
+        toDo1.setDescription(description);
+
+        System.out.println("Enter a task priority");
+        String priority = input.nextLine();
+        toDo1.setPriority(priority);
+
+        System.out.println("Task Completed? (Y or N)");
+        char completed = input.next().charAt(0);
+        toDo1.setCompleted(completed);
+
+        toDo1.getToDo();
     }
 }
