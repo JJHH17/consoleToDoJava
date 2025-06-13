eaa7778c0db03be833e6f00716166f93a8d0f895
James Hatfield <james.hatfield17@icloud.com>
2025-06-13 20:22:52 +0100

    Added loop to main class which will serve as how user interacts with program

--- src/todo/Main.java
+++ src/todo/Main.java
@@ -5,31 +5,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Global variable for ID tracking
-        int id = 0;
-        // Creating an instance
-        ToDo toDo1 = new ToDo(id + 1);
-
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Welcome to our To Do tracker!");
-        System.out.println();
+        System.out.println("Welcome to this To Do application!");
+        System.out.println("__________________________________");
 
-        // Initial prompt for user
-        System.out.println("Please create your first To Do!");
+        // Creating initial program loop
+        while (true) {
+            System.out.println("Add a new item (add), view an existing item (view), or quit (quit)");
+            String continuePrompt = input.next();
 
-        System.out.println("Enter a task description");
-        String description = input.nextLine();
-        toDo1.setDescription(description);
+            // Exit criteria of code
+            if (continuePrompt.toLowerCase().contains("quit")) {
+                break;
+            }
 
-        System.out.println("Enter a task priority");
-        String priority = input.nextLine();
-        toDo1.setPriority(priority);
+            switch (continuePrompt.toLowerCase()) {
+                case "add":
+                    System.out.println("to be continued");
+                    break;
+                case "view":
+                    System.out.println("To also be continued");
+                    break;
+                default:
+                    System.out.println("Please enter a valid argument");
+            }
+        }
 
-        System.out.println("Task Completed? (Y or N)");
-        char completed = input.next().charAt(0);
-        toDo1.setCompleted(completed);
-
-        toDo1.getToDo();
     }
 }
